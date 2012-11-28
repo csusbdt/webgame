31a8b4e73a7e9db288f2d0c651d87bf7759f0484
turner <csusbdt@gmail.com>
2012-11-27 20:15:47 -0800

    made view adaptable

--- web/proto3/js/view.js
+++ web/proto3/js/view.js
@@ -1,23 +1,41 @@
 (function() {
 
+	//////////////////////////////////////////////////////////////////////////////
+	//
+	//  Backgrounds should be created with dimensions DRAW_AREA_WIDTH by 
+	//  DRAW_AREA_HEIGHT.  Drawing that is critical to game play should be
+	//  done within a rectangle centered in the draw area that has dimensions
+	//  GAME_PLAY_WIDTH by GAME_PLAY_HEIGHT.
+	//
+	//////////////////////////////////////////////////////////////////////////////
+
 	var canvas = document.getElementById('canvas'),
 		context = canvas.getContext('2d'),
 		drawableLayers = [ ],
 		WEIGHT = 0.03,
 		currentTime = new Date().getTime(),
 		previousTime = currentTime,
-		dt = 16;  // average milliseconds per frame
+		dt = 16,  // average milliseconds per frame
+		scale = 1,
+		offsetX = 0,
+		offsetY = 0,
+		canvasMarginLeft = 0,
+		canvasMarginRight = 0;
 
 	view = { 
-		width: null,
-		height: null,
-		scale: 1
+		DRAW_AREA_WIDTH: 1024,
+		DRAW_AREA_HEIGHT: 720,
+		GAME_PLAY_WIDTH: 800,
+		GAME_PLAY_HEIGHT: 600
 	};
 	
-	view.resize = function(width, height) {
-		canvas.width = view.width = width;
-		canvas.height = view.height = height;
-	};
+	view.getX = function(e) {
+		return (e.pageX - canvasMarginLeft) / scale + offsetX;
+	}
+	
+	view.getY = function(e) {
+		return (e.pageY - canvasMarginRight) / scale + offsetY;
+	}
 	
 	view.addDrawable = function(drawable, layerIndex, duration) {
 		var i;
@@ -57,7 +75,29 @@
 	view.removeAllDrawables = function() {
 		drawableLayers.length = 0;
 	};
-	
+
+	function adjustCanvas() {
+		var screenWidth = $(window).innerWidth();
+		var screenHeight = $(window).innerHeight();
+		
+		// Determine the scale needed to render game play area.
+		scale = Math.min(1, screenWidth / view.GAME_PLAY_WIDTH, screenHeight / view.GAME_PLAY_HEIGHT);
+		
+		// Determine canvas size.
+		canvas.width = Math.min(screenWidth, view.DRAW_AREA_WIDTH * scale);
+		canvas.height = Math.min(screenHeight, view.DRAW_AREA_HEIGHT * scale);
+
+		// Center the drawing area in the canvas.
+		offsetX = (canvas.width - view.DRAW_AREA_WIDTH * scale) / 2;
+		offsetY = (canvas.height - view.DRAW_AREA_HEIGHT * scale) / 2;
+		
+		// Center canvas in browser window.
+		canvasMarginLeft = (screenWidth - canvas.width) / 2;
+		canvasMarginRight = (screenHeight - canvas.height) / 2;
+		$('#canvas').css('margin-left', canvasMarginLeft);
+		$('#canvas').css('margin-top', canvasMarginRight);
+	};
+
 	//////////////////////////////////////////////////////////////////////////////
 	//
 	// Note: Javascript Date object does not give reliable time values, so we
@@ -70,8 +110,8 @@
 		var elapsedTime = currentTime - previousTime;
 		previousTime = currentTime;
 		dt = dt * (1 - WEIGHT) + elapsedTime * WEIGHT;
-		context.clearRect(0, 0, view.width, view.height);
-		context.setTransform(view.scale, 0, 0, view.scale, 0, 0);
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.setTransform(scale, 0, 0, scale, offsetX, offsetY);
 		for (var layerIndex = 0; layerIndex < drawableLayers.length; ++layerIndex) {
 			var layer = drawableLayers[layerIndex];
 			for (var i = 0; i < layer.length; ++i) {
@@ -81,7 +121,9 @@
 		context.setTransform(1, 0, 0, 1, 0, 0);
 		requestAnimationFrame(animationLoop);
 	};
-	
+
+	adjustCanvas();
+	$(window).resize(adjustCanvas);	
 	requestAnimationFrame(animationLoop);
 
 })();
--- web/proto3/maps/flex.js
+++ web/proto3/maps/flex.js
@@ -1,88 +1,37 @@
 (function() {
 
-	var MAX_WIDTH = 1024,
-		MAX_HEIGHT = 720,
-		MIN_WIDTH = 800,
-		MIN_HEIGHT = 200,
-		ASPECT_RATIO = MIN_WIDTH/MIN_HEIGHT,
-		background = { };
+	var groundHeight = view.DRAW_AREA_HEIGHT / 2,
+		pepperImage = new Image(),
+		pc = { },
+		floor = { },
+		background = { },
+		MOVE_SPEED = 0.1,   // pixels per millisecond
+		SNAP_DISTANCE = 2,  // pixels
+		MIN_X = (view.DRAW_AREA_WIDTH - view.GAME_PLAY_WIDTH) / 2,
+		MAX_X = view.DRAW_AREA_WIDTH - MIN_X;
 		
-	// Construct the background object.		
+	// Construct a test background object.		
 	(function() {
 		var canvas = document.createElement('canvas');
 		var context = canvas.getContext('2d');
-		canvas.width = MAX_WIDTH;
-		canvas.height = MAX_HEIGHT;
+		canvas.width = view.DRAW_AREA_WIDTH;
+		canvas.height = view.DRAW_AREA_HEIGHT;
 		background.image = canvas;
-		background.x = 0;
-		background.y = 0;		
 		context.fillStyle="#00FF00";
-		context.fillRect(0, 0, MAX_WIDTH, MAX_HEIGHT); 
+		context.fillRect(0, 0, view.DRAW_AREA_WIDTH, view.DRAW_AREA_HEIGHT); 
 		context.fillStyle="#FF0000";
-		context.fillRect((MAX_WIDTH - MIN_WIDTH)/2, (MAX_HEIGHT - MIN_HEIGHT)/2, MIN_WIDTH, MIN_HEIGHT); 		
+		context.fillRect(
+			(view.DRAW_AREA_WIDTH - view.GAME_PLAY_WIDTH) / 2, 
+			(view.DRAW_AREA_HEIGHT - view.GAME_PLAY_HEIGHT) / 2, 
+			view.GAME_PLAY_WIDTH,
+			view.GAME_PLAY_HEIGHT); 		
 		background.draw = function(dt, ctx) {
-			ctx.drawImage(background.image, background.x, background.y);
+			ctx.drawImage(background.image, 0, 0);
 		};
 	})();
 	
 	view.addDrawable(background);
 
-	function resizeCanvas() {
-		var canvasHeight;
-		var canvasWidth;
-		var canvasTopMargin = 0;
-		var canvasLeftMargin = 0;
-		var scale = 1;
-		var screenWidth = $(window).innerWidth();
-		var screenHeight = $(window).innerHeight();
-
-		var	horizontalScale = screenWidth / MIN_WIDTH;
-		var verticalScale = screenHeight / MIN_HEIGHT;
-		var scale = Math.min(horizontalScale, verticalScale, 1);
-
-		if (horizontalScale < verticalScale && horizontalScale < 1) {
-			// case 1
-			view.scale = horizontalScale;
-			canvasWidth = screenWidth;
-			canvasHeight = screenHeight;
-			background.x = (MIN_WIDTH - MAX_WIDTH) / 2;
-			background.y = (MIN_HEIGHT - MAX_HEIGHT) / 2;
-		} else if (verticalScale < horizontalScale && verticalScale < 1) {
-			// case 2
-			
-		} else if (screenWidth < MAX_WIDTH || screenHeight < MAX_HEIGHT) {
-			// case 3
-			view.scale = 1;
-			canvasWidth = screenWidth;
-			canvasHeight = screenHeight;
-			background.x = (screenWidth - MAX_WIDTH) / 2;
-			background.y = (screenHeight - MAX_HEIGHT) / 2;
-		} else {
-			// case 4
-			view.scale = 1;
-			canvasLeftMargin = (screenWidth - MAX_WIDTH) / 2;
-			canvasTopMargin = (screenHeight - MAX_HEIGHT) / 2;
-			canvasWidth = MAX_WIDTH;
-			canvasHeight = MAX_HEIGHT;
-		}
-		$('#canvas').css('margin-left', canvasLeftMargin);
-		$('#canvas').css('margin-top', canvasTopMargin);
-		view.resize(canvasWidth, canvasHeight);
-	}
-	
-	resizeCanvas();
-	$(window).resize(resizeCanvas);
-
-// 	var timeout = null;
-// 	$(window).resize(function() {
-// 		clearTimeout(timeout);			
-// 		timeout = setTimeout(function() {
-// 			timeout = null;
-// 			resizeCanvas();
-// 		}, 1000);
-// 	});
-
-/*
 	pepperImage.src = 'images/pepper.png';
 	pc.draw = function(dt, ctx) {
 		var delta = MOVE_SPEED * dt;
@@ -93,21 +42,22 @@
 			pc.x -= delta;
 			if (pc.x < pc.destX + SNAP_DISTANCE) pc.x = pc.destX;
 		}
-		ctx.drawImage(pepperImage, pc.x, pc.y - 32);
+		ctx.drawImage(pepperImage, pc.x - 16, pc.y - 32);
 	}
-*/
-/*
-	pc.x = 0;
+	
+	pc.x = view.DRAW_AREA_WIDTH / 2;
 	pc.destX = pc.x;
-	pc.y = view.height - floorHeight;
+	pc.y = groundHeight;
 	pc.destY = pc.y;
+
+	view.addDrawable(pc);
 
 	floor.draw = function(dt, ctx) {
 		ctx.save();
 		ctx.lineWidth = 5;
 		ctx.beginPath();
-		ctx.moveTo(0, view.height - floorHeight);
-		ctx.lineTo(view.width, view.height - floorHeight);
+		ctx.moveTo(0, groundHeight);
+		ctx.lineTo(view.DRAW_AREA_WIDTH, groundHeight);
 		ctx.stroke();
 		ctx.restore();
 	};
@@ -115,10 +65,16 @@
 	view.addDrawable(pc, 1);
 	view.addDrawable(floor);  // default layer is 0
 	
-	$('#content').click(function(e) {
-		pc.destX = e.pageX - 16;
+	$(document).click(function(e) {
+		var x = view.getX(e);
+		if (x < MIN_X + 16) {
+			pc.destX = MIN_X + 16;
+		} else if (x > MAX_X - 16) {
+			pc.destX = MAX_X - 16;
+		} else {
+			pc.destX = x;
+		}
 	});
-*/
 
 	map.endLoad();	
 
